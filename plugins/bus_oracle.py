3af79a147fc8104d2e0bda6ddd496782c52554d5
halv00rsen <jorgen-halvorsen@hotmail.com>
2015-08-20 17:01:49 +0200

    Uses atb-oracle api to get bus data

---
+++ plugins/bus_oracle.py
@@ -0,0 +1,15 @@
+#! python3
+from urllib.request import urlopen, quote
+
+outputs[]
+channels = ["C08R9TE2F"] # radioteknisk
+
+api = "http://m.atb.no/xmlhttprequest.php?service=routeplannerOracle.getOracleAnswer&question="
+
+def process_message(data):
+	if data["channel"] in channels:
+		if data["text"].startswith(".bus "):
+			info = data["text"][5:]
+			if len(info) < 1:
+				return
+			outputs.append(urlopen(api + quote(info)).read().decode())
