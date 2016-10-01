7bd018be69beeeb077c91592bbb56f3dda1b5a11
Thorben Dahl <thorben@sjostrom.no>
2016-10-01 16:16:27 +0200

    Convert tabs to spaces in fun_facts.py
    I stumbled across the tabs when testing the previous commit.

--- plugins/fun_facts.py
+++ plugins/fun_facts.py
@@ -11,13 +11,13 @@ replace = {'\\"': '"'}
 remove = ["<strong>", "</strong>", "<em>", "</em>"]
 
 def process_message(data):
-	if data["text"] in [".fun", ".funfact", ".fact"]:
-		info = "[]"
-		while info == "[]":
-			info = urlopen(url + str(random.randint(bottom, top))).read().decode()
-		fact = info.split("</p>")[0][12:]
-		for a in replace:
-			fact = fact.replace(a, replace[a])
-		for a in remove:
-			fact = fact.replace(a, "")
-		outputs.append([data["channel"], fact])
+    if data["text"] in [".fun", ".funfact", ".fact"]:
+        info = "[]"
+        while info == "[]":
+            info = urlopen(url + str(random.randint(bottom, top))).read().decode()
+        fact = info.split("</p>")[0][12:]
+        for a in replace:
+            fact = fact.replace(a, replace[a])
+        for a in remove:
+            fact = fact.replace(a, "")
+        outputs.append([data["channel"], fact])
