04e111ff36d1c2769bca74d4271cbaa996dd2bae
Thorben Dahl <thorben@sjostrom.no>
2017-02-23 23:26:28 +0100

    Fix mising Plugin in class name

--- plugins/shutup_slackbot.py
+++ plugins/shutup_slackbot.py
@@ -1,7 +1,7 @@
 from rtmbot.core import Plugin
 
 
-class ShutupSlackbot(Plugin):
+class ShutupSlackbotPlugin(Plugin):
     def process_message(self, data):
         if data["text"] in ["Bpye Berg Nyberg"]:
             self.outputs.append([data["channel"], "Hold kjeft, slackbot..."])
